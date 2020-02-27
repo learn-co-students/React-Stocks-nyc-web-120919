7164dd084c4349b6d8ab1334da11c43743be53fa
merewelch214 <merewelch@gmail.com>
2020-02-27 14:26:08 -0500

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -7,9 +7,7 @@ class PortfolioContainer extends Component {
     return (
       <div>
         <h2>My Portfolio</h2>
-          {
-            //render your portfolio stocks here
-          }
+          {this.props.stocks.map(stock => <Stock stock={stock} key={stock.id} handleClick={this.props.sellStock}/>)}
       </div>
     );
   }
